062fb584b736a43b75fa1aae105fe0d952b61bcd
koyo_hirose <k_hirose@system-audit.co.jp>
2025-09-30 16:46:07 +0900

    Analyticsの設定と要件定義書更新

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
       {/* スクロール時のバウンスを抑制する */}
       <body className={`${inter.className} overscroll-none`}>
         {children}
+        <Analytics />
       </body>
       {/* --- 【ここまで】 --- */}
     </html>
